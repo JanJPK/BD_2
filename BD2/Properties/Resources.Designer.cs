bebb72033f5bd974a684f5aeb4bd9a9f518d4994
Jan Pajdak <pajdakjan@gmail.com>
2017-11-07 22:30:43 +0100

    backup before remaking gui

--- BD2/Properties/Resources.Designer.cs
+++ BD2/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace BD2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap tank_star {
+            get {
+                object obj = ResourceManager.GetObject("tank_star", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
